28fb384602e7e85df052a1babd57cee137b6f2dc
Kiyanzade <s.Kiyanzade@gmail.com>
2024-08-28 22:18:54 +0330

    add login action and Swagger Auth with token

--- ProductApi/Controllers/AuthController .cs
+++ ProductApi/Controllers/AuthController .cs
@@ -50,6 +50,36 @@ namespace ProductApi.Controllers
         }
 
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user == null || !(await _userManager.CheckPasswordAsync(user, model.Password)))
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
 
     }
 }
--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ProductApi.Models;
 using ProductAPI.Data;
 using System.Text;
@@ -20,7 +21,31 @@ namespace ProductApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                // Authorize in swagger with Token
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+            {
+                new OpenApiSecurityScheme {
+                    Reference = new OpenApiReference {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+            });
             builder.Services.AddAuthorization();
 
             builder.Services.AddDbContext<ProductContext>(options =>
@@ -28,11 +53,13 @@ namespace ProductApi
 
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
             {
-                /*options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredLength = 6;*/
+                // for comfort during registration
+                options.Password.RequireDigit = false; 
+                options.Password.RequiredLength = 6; 
+                options.Password.RequireNonAlphanumeric = false; 
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false; 
+                options.Password.RequiredUniqueChars = 1; 
             })
                 .AddEntityFrameworkStores<ProductContext>()
                 .AddDefaultTokenProviders();
