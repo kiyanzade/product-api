06f8f66d6e7a490e27a2b4207825b0deab27bd07
Kiyanzade <s.Kiyanzade@gmail.com>
2024-08-28 16:20:16 +0330

    Add project files.

---
+++ ProductApi/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductAPI.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ProduceDate { get; set; }
+
+        [Required]
+        [Phone]
+        public string ManufacturePhone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string ManufactureEmail { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+}
